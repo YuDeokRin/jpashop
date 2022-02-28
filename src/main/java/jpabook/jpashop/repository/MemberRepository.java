43c3c5b3219fa164eb3368329c47613c48fda5f8
YuDeokRin <ejrfls12@naver.com>
2022-02-28 17:59:33 +0900

    회원 도메인 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,29 +1,28 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member){
         em.persist(member);
     }
 
-
-    public Member find(Long id){
+    public Member findOne(Long id){
         return em.find(Member.class, id);
     }
 
     public List<Member> findAll() {
-        return em.createQuery("select  m from Member", Member.class)
+        return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
